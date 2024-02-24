f2a8d92b061d7cd1676958418ea3903a3cd0dea3
Ryan Loftus <ryanloftus02@outlook.com>
2024-02-24 18:53:15 -0500

    adding passcode verification with argon2 hash

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,23 +1,12 @@
 import streamlit as st
 import os
 import os.path
-import argon2
 import storage
-from twofish import Twofish
 
 if not os.path.isdir(storage.PW_DIR):
     new_passcode_input = st.text_input(label="Create a passcode")
     if new_passcode_input:
-        key = argon2.hash_password(new_passcode_input.encode(), hash_len=32).decode()
-        t = Twofish(new_passcode_input.encode())
-        accounts_file_contents = storage.encrypt(t, b'{"accounts": []}')
-        sso_file_contents = storage.encrypt(t, b'{"sso": []}')
-        os.mkdir(storage.PW_DIR)
-        with open(storage.ACCOUNTS_FILE, "wb") as f:
-            f.write(accounts_file_contents)
-        with open(storage.SSO_FILE, "wb") as f:
-            f.write(sso_file_contents)
-        storage.key = new_passcode_input.encode()
+        storage.create(new_passcode_input)
 
 if os.path.isdir(storage.PW_DIR):
     if storage.key is None:
--- streamlit/storage.py
+++ streamlit/storage.py
@@ -1,14 +1,33 @@
 import os
 import json
 from twofish import Twofish
+from argon2 import PasswordHasher
 
 PW_DIR = os.getenv("HOME") + "/.pw_manager"
 ACCOUNTS_FILE = PW_DIR + "/accounts.json"
 SSO_FILE = PW_DIR + "/sso.json"
+HASH_FILE = PW_DIR + "/hash.txt"
 
 key: bytes = None
+ph: PasswordHasher = PasswordHasher(hash_len=32)
 
 BLOCK_SIZE = 16
+
+def create(passcode: str):
+    global key, ph
+    passcode = passcode.encode()
+    hash = ph.hash(passcode)
+    t = Twofish(passcode)
+    accounts_file_contents = encrypt(t, b'{"accounts": []}')
+    sso_file_contents = encrypt(t, b'{"sso": []}')
+    os.mkdir(PW_DIR)
+    with open(HASH_FILE, "w") as f:
+        f.write(hash)
+    with open(ACCOUNTS_FILE, "wb") as f:
+        f.write(accounts_file_contents)
+    with open(SSO_FILE, "wb") as f:
+        f.write(sso_file_contents)
+    key = passcode
 
 def encrypt(t: Twofish, plaintext: bytes):
     while len(plaintext) % BLOCK_SIZE != 0:
@@ -29,12 +48,14 @@ def decrypt(t: Twofish, ciphertext: bytes):
     return plaintext.decode().strip()
 
 def get_file_contents(file):
+    global key
     with open(file, "rb") as f:
         file_contents = f.read()
     t = Twofish(key)
     return json.loads(decrypt(t, file_contents))
 
 def write_file_contents(file, data):
+    global key
     t = Twofish(key)
     file_contents = json.dumps(data)
     encrypted_file_contents = encrypt(t, file_contents.encode())
@@ -42,4 +63,6 @@ def write_file_contents(file, data):
         f.write(encrypted_file_contents)
 
 def is_correct_key(key: str):
-    return True
+    global ph
+    with open(HASH_FILE, "rb") as f:
+        return ph.verify(f.read(), key)
